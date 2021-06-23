7a9484cc1541f96c1b3a6c1c1bdfaa5e1e701d16
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-06-23 21:49:56 +0500

    Added generate hash method (#3)

--- block.py
+++ block.py
@@ -1,9 +1,26 @@
 from datetime import datetime
+from hashlib import sha256
 
 
 class Block:
-    def __init__(self, transactions, previous_hash):
+    def __init__(self, transactions, previous_hash, nonce=0):
+        self.timestamp = datetime.now()
         self.transactions = transactions
         self.previous_hash = previous_hash
-        self.nonce = 0
-        self.timestamp = datetime.now()
+        self.nonce = nonce
+        self.hash = self.generate_hash()
+
+    def print_block(self):
+        print("timestamp:", self.timestamp)
+        print("transactions:", self.transactions)
+        print("current hash:", self.generate_hash())
+
+    def generate_hash(self):
+        block_contents = (
+            str(self.timestamp)
+            + str(self.transactions)
+            + str(self.previous_hash)
+            + str(self.nonce)
+        )
+        block_hash = sha256(block_contents.encode())
+        return block_hash.hexdigest()
