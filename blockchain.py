d565d9ec55ca94bbbee955ee37d16c7cb96b192a
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-06-23 21:55:40 +0500

    Adedd Blockchain class (#4)

---
+++ blockchain.py
@@ -0,0 +1,11 @@
+from block import Block
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.all_transactions = []
+        self.genesis_block()
+
+    def genesis_block(self):
+        self.chain.append(Block([], "0"))
